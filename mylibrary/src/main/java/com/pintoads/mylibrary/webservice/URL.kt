99355a153b1ef660d136930a6749a2dc88dbab64
AvidA <AtrasVidA@gmail.com>
2020-05-05 12:54:46 +0430

    fc4

--- mylibrary/src/main/java/com/pintoads/mylibrary/webservice/URL.kt
+++
@@ -1,7 +0,0 @@
-package com.pintoads.himasdk.webservice
-
-object URL {
-    //const val BASE_URL = "http://172.16.100.141:8000"
-    const val BASE_URL = "http://51.91.204.76:5000"
-    const val IMAGE_URL =""// BASE_URL
-}
