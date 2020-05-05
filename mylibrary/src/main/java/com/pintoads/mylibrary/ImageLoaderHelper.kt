c39cae67faa619cff3b994f67bd83ea7454465bb
AvidA <AtrasVidA@gmail.com>
2020-05-05 13:10:22 +0430

    fc5

--- mylibrary/src/main/java/com/pintoads/mylibrary/ImageLoaderHelper.kt
+++ mylibrary/src/main/java/com/pintoads/mylibrary/ImageLoaderHelper.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
-import jp.wasabeef.glide.transformations.CropCircleTransformation
 
 
 object ImageLoaderHelper {
@@ -12,7 +11,7 @@ object ImageLoaderHelper {
         Glide.with(context)
             .load(imageUrl)
             .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
-            .transform(CropCircleTransformation())
+
             .into(imageView)
     }
 
